174b24740cd99eb743d9166a6b9ef77c4146cc90
Sverine <severine.sylvan@gmail.com>
2021-12-13 18:33:34 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route,  } from 'react-router-dom';
+import { Routes, Route,  } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import News from './pages/News';
@@ -7,14 +7,12 @@ import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/a-propos" element={<About/>}/>
-        <Route path="/news" element={<News/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/a-propos" element={<About/>}/>
+      <Route path="/news" element={<News/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles/index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<HashRouter>
+    <React.StrictMode>
+      <App />
+  </React.StrictMode>
+</HashRouter>,
   document.getElementById('root')
 );
 
