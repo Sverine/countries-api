a876f7cae001e89bfdba31e4f388779020303a5c
Sverine <severine.sylvan@gmail.com>
2021-12-13 18:19:46 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route,  } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import News from './pages/News';
@@ -7,11 +7,11 @@ import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/countries-api/" element={<Home/>}/>
-        <Route path="/countries-api/a-propos" element={<About/>}/>
-        <Route path="/countries-api/news" element={<News/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/a-propos" element={<About/>}/>
+        <Route path="/news" element={<News/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
