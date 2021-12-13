b5e07eee21064b3d0b341869839186c96fe2cae8
Sverine <severine.sylvan@gmail.com>
2021-12-13 21:27:59 +0100

    hide the news page from navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
         <div>
             <div className="navigation">
                 <NavLink className={({isActive})=> isActive ? "nav-active":""} to="/">Accueil</NavLink>
-                <NavLink className={({isActive})=> isActive ? "nav-active":""}  to="/news">News</NavLink>
+                {/* <NavLink className={({isActive})=> isActive ? "nav-active":""}  to="/news">News</NavLink> */}
                 <NavLink className={({isActive})=> isActive ? "nav-active":""}  to="/a-propos">A propos</NavLink>
             </div>
         </div>
